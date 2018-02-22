63e8108e0a8dd6182ae393b6cbe1625f50b5b547
carlogilmar <carlo@makingdevs.com>
2018-02-22 15:21:14 -0600

    coding RD-129 [client] Adding action in route find-users and getting input field

--- app/routes/invite-module/find-users.js
+++ app/routes/invite-module/find-users.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  actions:{
+
+    findUsername:function(){
+      let username = this.get('controller').get('username');
+      this.get('controller').set('username', '');
+    }
+
+  }
+
 });
