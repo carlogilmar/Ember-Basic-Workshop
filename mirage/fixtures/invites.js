c80247e1e6d2499eba26422e3ade42b59330006b
Carlo Gilmar <carlogilmar12@gmail.com>
2018-02-22 01:15:44 -0600

    ex-C Mirage 1/4 Add invite fixtures (fake data)

---
+++ mirage/fixtures/invites.js
@@ -0,0 +1,12 @@
+export default [
+  {id:1, user:"fake user 1", dateCreated:1519283297836, status:"invited"},
+  {id:2, user:"fake user 2", dateCreated:1519283297836, status:"canceled"},
+  {id:3, user:"fake user 3", dateCreated:1519283297836, status:"reinvited"},
+  {id:4, user:"fake user 4", dateCreated:1519283297836, status:"invited"},
+  {id:5, user:"fake user 5", dateCreated:1519283297836, status:"reinvited"},
+  {id:6, user:"fake user 6", dateCreated:1519283297836, status:"invited"},
+  {id:7, user:"fake user 7", dateCreated:1519283297836, status:"answered"},
+  {id:8, user:"fake user 8", dateCreated:1519283297836, status:"answered"},
+  {id:9, user:"fake user 9", dateCreated:1519283297836, status:"invited"},
+  {id:10, user:"fake user 10", dateCreated:1519283297836, status:"answered"}
+];
