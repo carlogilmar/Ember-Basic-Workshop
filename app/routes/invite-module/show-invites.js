fd916e69ed794ac9395ec8006fbc39ccc5e30779
Carlo Gilmar <carlogilmar12@gmail.com>
2018-02-24 02:41:49 -0600

    Update invite status

--- app/routes/invite-module/show-invites.js
+++ app/routes/invite-module/show-invites.js
@@ -1,7 +1,41 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
   model(){
     return this.get('store').findAll('invite');
+  },
+
+  actions:{
+    reInvite:function(invite){
+      let currentInvite = invite
+      currentInvite.set('status', 'reinvited');
+      currentInvite.save().then( ()=>{
+        alert("Invited mark as REINVITED!!");
+      },
+        ()=>{
+          alert("Error!");
+        });
+    },
+    answerInvite:function(invite){
+      let currentInvite = invite
+      currentInvite.set('status', 'answered');
+      currentInvite.save().then( ()=>{
+        alert("Invited mark as ANSWERED!!");
+      },
+        ()=>{
+          alert("Error!");
+        });
+    },
+    cancelInvite:function(invite){
+      let currentInvite = invite
+      currentInvite.set('status', 'canceled');
+      currentInvite.save().then( ()=>{
+        alert("Invited mark as CANCELED!!");
+      },
+        ()=>{
+          alert("Error!");
+        });
+    }
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -2,6 +2,7 @@ export default function() {
 
   this.get('/invites');
   this.post('/invites');
+  this.put('/invites/:id');
 
   this.get('/users');
   this.post('/users');
