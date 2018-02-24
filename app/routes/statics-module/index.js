38fee79846cf3ed0d36815aafd355d201f060906
Carlo Gilmar <carlogilmar12@gmail.com>
2018-02-24 02:54:40 -0600

    Getting all invites

--- app/routes/statics-module/index.js
+++ app/routes/statics-module/index.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+    return Ember.RSVP.hash({
+      invites:this.get('store').findAll('invite')
+    });
+  }
 });
