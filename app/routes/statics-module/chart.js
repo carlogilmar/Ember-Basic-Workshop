042cb730d7cbfbb92f39701d03a23f77df56a206
Carlo Gilmar <carlogilmar12@gmail.com>
2018-02-24 23:31:26 -0600

    Modify nested route for show chart

--- app/routes/statics-module/chart.js
+++ app/routes/statics-module/chart.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import RSVP from 'rsvp';
 
 export default Route.extend({
+  model(){
+    return RSVP.hash({
+      invites:this.get('store').findAll('invite')
+    });
+  }
 });
