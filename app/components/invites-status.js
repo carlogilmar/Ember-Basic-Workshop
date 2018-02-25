1e13e4a8b2b6d7260f5b2184eac4b81880bdc88e
Carlo Gilmar <carlogilmar12@gmail.com>
2018-02-24 23:46:07 -0600

    Adding component for show counter by invite status

--- app/components/invites-status.js
+++ app/components/invites-status.js
@@ -1,4 +1,35 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+
+  invitesStatus: computed('invites', function(){
+
+    let invites = this.get('invites').get('content');
+    let statusCounters = this.getStatusCounters(invites);
+    return statusCounters;
+
+  }),
+
+  getStatusCounters:function(invites){
+
+    let status = {"invited":0, "reinvited":0, "answered":0, "canceled":0};
+    let invitesSize = invites.length
+
+    for(var index=0; index < invitesSize; index++){
+      var currentRecord = invites.pop();
+      status[currentRecord.__data.status]++;
+    }
+
+    var statusCounters = [
+      {status:"Invited", counter:status["invited"]},
+      {status:"Reinvited", counter:status["reinvited"]},
+      {status:"Answered", counter:status["answered"]},
+      {status:"Canceled", counter:status["canceled"]}
+    ]
+
+    return statusCounters;
+
+  }
+
 });
